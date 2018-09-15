d37365bec2208da1f7b3040052d8470bfcf88667
travisgdesir31 <travisgdesir31@gmail.com>
2018-09-15 17:36:58 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,9 +30,10 @@ end
 
 def hit?(answer)
   prompt_user
-  if get_user_input == "h" 
+  answer = get_user_input 
+  if answer == "h" 
     deal_card
-  elsif get_user_input == "s"
+  elsif answer == "s"
   
   end
   
